c8a74bac9d03c71f591cb66e212ca44a8aa75b9f
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-03 21:33:26 +0200

    Add struct for range of date

---
+++ src/Zeus.Models/Utilities/DateRange.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Zeus.Models.Utilities
+{
+   public readonly struct DateRange
+   {
+      public DateTime Start { get; init; }
+      public DateTime End { get; init; }
+   }
+}
