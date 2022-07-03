9f6b2a5fe8cb8269f0b20d59f0ca680a46f6494a
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-04 00:52:06 +0200

    Database configuration for locations

--- src/Zeus.Infrastructure/Repositories/Configurations/MongoConfiguration.cs
+++ src/Zeus.Infrastructure/Repositories/Configurations/MongoConfiguration.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using Zeus.Domain.Locations;
 using Zeus.Domain.Users;
 using Zeus.Enums.Users;
 using Zeus.Utilities.Extensions;
@@ -18,8 +19,41 @@ namespace Zeus.Infrastructure.Repositories.Configurations
 
       private static async Task CreateIndexesAsync(IMongoDatabase database)
       {
+         await LocationIndexesAsync(database);
+         await LocationHistoryIndexesAsync(database);
+
          await CreateUserIndexesAsync(database);
          await CreateUserHistoryIndexesAsync(database);
+      }
+
+      private static Task LocationIndexesAsync(IMongoDatabase database)
+      {
+         List<CreateIndexModel<Location>> list = new()
+         {
+            new CreateIndexModel<Location>(
+               Builders<Location>.IndexKeys.Ascending(x => x.Name),
+               new CreateIndexOptions { Name = nameof(Location.Name), Unique = true, Background = true }),
+
+            new CreateIndexModel<Location>(
+               Builders<Location>.IndexKeys.Ascending(x => x.MacAddress),
+               new CreateIndexOptions { Name = nameof(Location.MacAddress), Unique = true, Background = true })
+         };
+
+         return CreateIndexesAsync(list, database);
+      }
+
+      private static Task LocationHistoryIndexesAsync(IMongoDatabase database)
+      {
+         List<CreateIndexModel<LocationHistory>> list = new()
+         {
+            new CreateIndexModel<LocationHistory>(Builders<LocationHistory>.IndexKeys.Combine(
+               Builders<LocationHistory>.IndexKeys.Ascending(x => x.LocationId),
+               Builders<LocationHistory>.IndexKeys.Ascending(x => x.CreateDate)
+            ),
+            new CreateIndexOptions { Name = $"{nameof(LocationHistory.LocationId)}-{nameof(LocationHistory.CreateDate)}", Unique = true, Background = true })
+         };
+
+         return CreateIndexesAsync(list, database);
       }
 
       private static Task CreateUserIndexesAsync(IMongoDatabase database)
