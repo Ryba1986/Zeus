b637af5b0d758245f35f86351064a950c3fc9f5e
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-03 14:38:06 +0200

    Create helpers for utilities

---
+++ src/Zeus.Utilities/Helpers/NetworkHelper.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zeus.Utilities.Helpers
+{
+   public static class NetworkHelper
+   {
+      public static string GetMacAddress()
+      {
+         return NetworkInterface
+            .GetAllNetworkInterfaces()
+            .FirstOrDefault(x =>
+               x.OperationalStatus == OperationalStatus.Up &&
+               x.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+               x.Speed > 1024
+            )?
+            .GetPhysicalAddress()
+            .ToString()
+            .ToUpper() ?? string.Empty;
+      }
+
+      public async static Task<string> GetTorHost(string filePath, CancellationToken cancellationToken)
+      {
+         if (!File.Exists(filePath))
+         {
+            return string.Empty;
+         }
+
+         return (await File.ReadAllTextAsync(filePath, cancellationToken))
+            .Replace("\n", string.Empty)
+            .Replace("\r", string.Empty)
+            .Replace("\t", string.Empty)
+            .Trim() ?? string.Empty;
+      }
+   }
+}
