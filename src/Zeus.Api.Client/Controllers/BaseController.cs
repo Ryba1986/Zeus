3e590bc99c24e2b275f95a6de3925ce34f458d82
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-03 20:33:21 +0200

    Create base objects for api client

---
+++ src/Zeus.Api.Client/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Zeus.Models.Base;
+using Zeus.Utilities.Extensions;
+
+namespace Zeus.Api.Client.Controllers
+{
+   [Authorize]
+   public abstract class BaseController : ControllerBase
+   {
+      private readonly IMediator _mediator;
+
+      public BaseController(IMediator mediator)
+      {
+         _mediator = mediator;
+      }
+
+      protected async Task<IActionResult> SendAsync<T>(IRequest<T> request) where T : class
+      {
+         return Ok(await _mediator.Send(request));
+      }
+
+      protected async Task<IActionResult> SendAsync(IRequest<Result> request)
+      {
+         return ModelState.IsValid
+            ? Ok(await _mediator.Send(request))
+            : BadRequest(ModelState);
+      }
+
+      protected int GetLocationId()
+      {
+         return User.Claims.GetNameValue();
+      }
+   }
+}
---
+++ src/Zeus.Api.Client/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Text;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Zeus.Enums.Tokens;
+using Zeus.Utilities.Extensions;
+using Zeus.Validators.Users;
+
+namespace Zeus.Api.Client
+{
+   internal sealed class Startup
+   {
+      public IConfiguration Configuration { get; }
+
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
+
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services
+            .AddControllers()
+            .AddJsonOptions(x =>
+            {
+               x.JsonSerializerOptions.WriteIndented = false;
+               x.JsonSerializerOptions.PropertyNamingPolicy = null;
+            })
+            .AddFluentValidation(config =>
+            {
+               config.RegisterValidatorsFromAssemblyContaining<CreateUserValidator>();
+               config.DisableDataAnnotationsValidation = true;
+            });
+
+         services
+            .AddAuthentication(x =>
+            {
+               x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+               x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+               x.TokenValidationParameters = new TokenValidationParameters
+               {
+                  ValidateLifetime = true,
+                  ValidateAudience = false,
+                  ValidateIssuer = true,
+                  ValidIssuer = JwtIssuer.Client.GetDescription(),
+                  ClockSkew = TimeSpan.Zero,
+                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"] ?? string.Empty))
+               };
+            });
+      }
+
+      public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+         }
+
+         app.UseRouting()
+            .UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto })
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseEndpoints(x => { x.MapDefaultControllerRoute(); });
+      }
+   }
+}
