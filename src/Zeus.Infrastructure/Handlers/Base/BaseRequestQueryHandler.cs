fe8675a14be73ac63098de4dc4dd6f8793033344
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-06 21:40:41 +0200

    Migrate to sql database (part 6)

--- src/Zeus.Infrastructure/Handlers/Base/BaseRequestQueryHandler.cs
+++ src/Zeus.Infrastructure/Handlers/Base/BaseRequestQueryHandler.cs
@@ -1,13 +1,13 @@
-using AutoMapper;
+using Mapster;
 using Zeus.Infrastructure.Repositories;
 
 namespace Zeus.Infrastructure.Handlers.Base
 {
    internal abstract class BaseRequestQueryHandler : BaseRequestHandler
    {
-      protected readonly IMapper _mapper;
+      protected readonly TypeAdapterConfig _mapper;
 
-      public BaseRequestQueryHandler(UnitOfWork uow, IMapper mapper) : base(uow)
+      public BaseRequestQueryHandler(UnitOfWork uow, TypeAdapterConfig mapper) : base(uow)
       {
          _mapper = mapper;
       }
