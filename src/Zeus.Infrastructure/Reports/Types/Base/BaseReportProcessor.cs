7e12926f53f47cbfacebd582b4af26f94ca84a56
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-10 12:59:52 +0200

    Fix starting point

--- src/Zeus.Infrastructure/Reports/Types/Base/BaseReportProcessor.cs
+++ src/Zeus.Infrastructure/Reports/Types/Base/BaseReportProcessor.cs
@@ -11,7 +11,7 @@ namespace Zeus.Infrastructure.Reports.Types.Base
       {
          StartingPoints = new()
          {
-            MeterColumn = 33,
+            MeterColumn = 35,
             PlcColumn = 2,
             Row = 8
          };
