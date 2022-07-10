5aafcdbea0db27dc371154c078b8fe15b770e682
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-10 15:11:09 +0200

    Remove unused properties

--- src/Zeus.Models/Devices/Dto/DeviceReportDto.cs
+++ src/Zeus.Models/Devices/Dto/DeviceReportDto.cs
@@ -1,4 +1,3 @@
-using Zeus.Enums.Devices;
 using Zeus.Enums.Plcs;
 using Zeus.Models.Base.Dto;
 
@@ -8,9 +7,7 @@ namespace Zeus.Models.Devices.Dto
    {
       public int LocationId { get; init; }
       public string Name { get; init; }
-      public DeviceType Type { get; init; }
       public PlcType PlcType { get; init; }
-      public bool IsPlc { get; init; }
       public bool IsCo1 { get; init; }
       public bool IsCo2 { get; init; }
       public bool IsCwu { get; init; }
