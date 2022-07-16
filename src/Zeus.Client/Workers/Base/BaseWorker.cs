a7a7d6d95f6c803bac3c8d1d4c4aa00edfe7df2a
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-16 13:03:38 +0200

    Calculate plc refresh interval

--- src/Zeus.Client/Workers/Base/BaseWorker.cs
+++ src/Zeus.Client/Workers/Base/BaseWorker.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using MediatR;
 using Microsoft.Extensions.Hosting;
 using Zeus.Client.Settings;
@@ -13,6 +14,17 @@ namespace Zeus.Client.Workers.Base
       {
          _mediator = mediator;
          _settings = settings;
+      }
+
+      protected int GetPlcRefreshInterval(Stopwatch sw)
+      {
+         int diff = _settings.PlcRefreshInterval - (int)sw.ElapsedMilliseconds;
+         if (diff < 0)
+         {
+            return 0;
+         }
+
+         return diff;
       }
    }
 }
