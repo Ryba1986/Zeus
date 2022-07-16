aaadafdb291fd5c92c2c4a5f003bc64409163250
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-16 22:28:52 +0200

    Simplification of the configuration module

--- src/Zeus.Client/Configuration/ZeusModule.cs
+++ src/Zeus.Client/Configuration/ZeusModule.cs
@@ -1,6 +1,14 @@
 using Autofac;
+using MediatR.Extensions.Autofac.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Zeus.Client.Configuration.Modules;
+using RestSharp;
+using Zeus.Client.Modbus.Base;
+using Zeus.Client.Modbus.Climatixs;
+using Zeus.Client.Modbus.Meters;
+using Zeus.Client.Modbus.Rvds;
+using Zeus.Client.Settings;
+using Zeus.Enums.Plcs;
+using Zeus.Utilities.Extensions;
 
 namespace Zeus.Client.Configuration
 {
@@ -15,9 +23,53 @@ namespace Zeus.Client.Configuration
 
       protected override void Load(ContainerBuilder builder)
       {
-         builder.RegisterModule<ModbusModule>();
-         builder.RegisterModule<ServiceModule>();
-         builder.RegisterModule(new SettingsModule(_configuration));
+         RegisterMediator(builder);
+         RegisterModbus(builder);
+         RegisterRestClient(builder);
+         RegisterSettings(builder);
+      }
+
+      private void RegisterMediator(ContainerBuilder builder)
+      {
+         builder.RegisterMediatR(ThisAssembly);
+      }
+
+      private static void RegisterModbus(ContainerBuilder builder)
+      {
+         builder
+            .RegisterType<MeterProcessor>()
+            .Keyed<IModbusProcessor>(PlcType.Meter);
+
+         builder
+            .RegisterType<ClimatixProcessor>()
+            .Keyed<IModbusProcessor>(PlcType.Climatix);
+
+         builder
+            .RegisterType<Rvd145Processor>()
+            .Keyed<IModbusProcessor>(PlcType.Rvd145);
+      }
+
+      private static void RegisterRestClient(ContainerBuilder builder)
+      {
+         builder.Register((ZeusSettings settings) =>
+         {
+            RestClientOptions options = new()
+            {
+               BaseUrl = new(settings.ApiBaseUrl),
+               ThrowOnAnyError = false,
+            };
+
+            return new RestClient(options);
+         })
+         .AsSelf()
+         .SingleInstance();
+      }
+
+      private void RegisterSettings(ContainerBuilder builder)
+      {
+         builder
+            .RegisterInstance(_configuration.GetSettings<ZeusSettings>())
+            .SingleInstance();
       }
    }
 }
