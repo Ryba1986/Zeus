7fdbf5a6803b3127c99aa6af4259037e3af26d34
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-28 11:35:40 +0200

    Adding an opening and closing tag for a conditional statement

--- src/Zeus.Infrastructure/Sql/DatabaseSetup.sql
+++ src/Zeus.Infrastructure/Sql/DatabaseSetup.sql
@@ -1,199 +1,199 @@
 -- V1
 IF((SELECT COUNT(*) FROM [INFORMATION_SCHEMA].[TABLES] WHERE TABLE_NAME = '_DB') = 0)
+BEGIN
+   BEGIN TRANSACTION;
+   BEGIN TRY
 
-BEGIN TRANSACTION;
-BEGIN TRY
+      CREATE TABLE [_DB]
+      (
+         [Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+         [Version] INT UNIQUE NOT NULL,
+         [Date] DATETIME2 NOT NULL,
+      );
+      INSERT INTO [_DB] ([Version], [Date])
+      VALUES (1, CAST(GETDATE() AS datetime2(0)));
 
-   CREATE TABLE [_DB]
-   (
-      [Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
-      [Version] INT UNIQUE NOT NULL,
-      [Date] DATETIME2 NOT NULL,
-   );
+      CREATE TABLE [User]
+      (
+         [Id] INT IDENTITY(1, 1) NOT NULL,
+         [Name] NVARCHAR(50) NOT NULL,
+         [Email] NVARCHAR(50) NOT NULL,
+         [Password] NCHAR(64) NOT NULL,
+         [Role] TINYINT NOT NULL,
+         [IsActive] BIT NOT NULL,
+         [Version] ROWVERSION NOT NULL,
+         CONSTRAINT [PK_User_Id] PRIMARY KEY CLUSTERED ([Id]),
+         CONSTRAINT [UK_User_Email] UNIQUE ([Email]),
+      );
+      -- // TODO: move to other logic
+      INSERT INTO [User] ([Name], [Email], [Password], [Role], [IsActive])
+      VALUES (N'Administrator', N'admin@admin.com', N'E7D3E769F3F593DADCB8634CC5B09FC90DD3A61C4A06A79CB0923662FE6FAE6B', 255, 1);
 
-   INSERT INTO [_DB] ([Version], [Date])
-   VALUES (1, CAST(GETDATE() AS datetime2(0)));
+      CREATE TABLE [UserHistory]
+      (
+         [Id] INT IDENTITY(1, 1) NOT NULL,
+         [UserId] INT NOT NULL,
+         [Name] NVARCHAR(50) NOT NULL,
+         [Email] NVARCHAR(50) NOT NULL,
+         [Role] NVARCHAR(30) NOT NULL,
+         [IsActive] BIT NOT NULL,
+         [CreatedById] INT NOT NULL,
+         [CreateDate] DATETIME2 NOT NULL,
+         CONSTRAINT [PK_UserHistory_Id] PRIMARY KEY CLUSTERED ([Id]),
+         CONSTRAINT [FK_UserHistory_UserId] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
+         CONSTRAINT [FK_UserHistory_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [User]([Id]),
+         INDEX [IX_UserHistory_UserId] NONCLUSTERED ([UserId]),
+      );
 
-   CREATE TABLE [User]
-   (
-      [Id] INT IDENTITY(1, 1) NOT NULL,
-      [Name] NVARCHAR(50) NOT NULL,
-      [Email] NVARCHAR(50) NOT NULL,
-      [Password] NCHAR(64) NOT NULL,
-      [Role] TINYINT NOT NULL,
-      [IsActive] BIT NOT NULL,
-      [Version] ROWVERSION NOT NULL,
-      CONSTRAINT [PK_User_Id] PRIMARY KEY CLUSTERED ([Id]),
-      CONSTRAINT [UK_User_Email] UNIQUE ([Email]),
-   );
-   -- // TODO: move to other logic
-   INSERT INTO [User] ([Name], [Email], [Password], [Role], [IsActive])
-   VALUES (N'Administrator', N'admin@admin.com', N'E7D3E769F3F593DADCB8634CC5B09FC90DD3A61C4A06A79CB0923662FE6FAE6B', 255, 1);
+      CREATE TABLE [Location]
+      (
+         [Id] INT IDENTITY(1, 1) NOT NULL,
+         [Name] NVARCHAR(30) NOT NULL,
+         [MacAddress] NVARCHAR(12) NOT NULL,
+         [Hostname] NVARCHAR(62) NOT NULL,
+         [ClientVersion] NVARCHAR(20) NOT NULL,
+         [IncludeReport] BIT NOT NULL,
+         [IsActive] BIT NOT NULL,
+         [Version] ROWVERSION NOT NULL,
+         CONSTRAINT [PK_Location_Id] PRIMARY KEY CLUSTERED ([Id]),
+         CONSTRAINT [UK_Location_Name] UNIQUE ([Name]),
+         CONSTRAINT [UK_Location_MacAddress] UNIQUE ([MacAddress]),
+      );
 
-   CREATE TABLE [UserHistory]
-   (
-      [Id] INT IDENTITY(1, 1) NOT NULL,
-      [UserId] INT NOT NULL,
-      [Name] NVARCHAR(50) NOT NULL,
-      [Email] NVARCHAR(50) NOT NULL,
-      [Role] NVARCHAR(30) NOT NULL,
-      [IsActive] BIT NOT NULL,
-      [CreatedById] INT NOT NULL,
-      [CreateDate] DATETIME2 NOT NULL,
-      CONSTRAINT [PK_UserHistory_Id] PRIMARY KEY CLUSTERED ([Id]),
-      CONSTRAINT [FK_UserHistory_UserId] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
-      CONSTRAINT [FK_UserHistory_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [User]([Id]),
-      INDEX [IX_UserHistory_UserId] NONCLUSTERED ([UserId]),
-   );
+      CREATE TABLE [LocationHistory]
+      (
+         [Id] INT IDENTITY(1, 1) NOT NULL,
+         [LocationId] INT NOT NULL,
+         [Name] NVARCHAR(30) NOT NULL,
+         [MacAddress] NVARCHAR(12) NOT NULL,
+         [IncludeReport] BIT NOT NULL,
+         [IsActive] BIT NOT NULL,
+         [CreatedById] INT NOT NULL,
+         [CreateDate] DATETIME2 NOT NULL,
+         CONSTRAINT [PK_LocationHistory_Id] PRIMARY KEY CLUSTERED ([Id]),
+         CONSTRAINT [FK_LocationHistory_LocationId] FOREIGN KEY ([LocationId]) REFERENCES [Location]([Id]),
+         CONSTRAINT [FK_LocationHistory_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [User]([Id]),
+         INDEX [IX_LocationHistory_LocationId] NONCLUSTERED ([LocationId]),
+      );
 
-   CREATE TABLE [Location]
-   (
-      [Id] INT IDENTITY(1, 1) NOT NULL,
-      [Name] NVARCHAR(30) NOT NULL,
-      [MacAddress] NVARCHAR(12) NOT NULL,
-      [Hostname] NVARCHAR(62) NOT NULL,
-      [ClientVersion] NVARCHAR(20) NOT NULL,
-      [IncludeReport] BIT NOT NULL,
-      [IsActive] BIT NOT NULL,
-      [Version] ROWVERSION NOT NULL,
-      CONSTRAINT [PK_Location_Id] PRIMARY KEY CLUSTERED ([Id]),
-      CONSTRAINT [UK_Location_Name] UNIQUE ([Name]),
-      CONSTRAINT [UK_Location_MacAddress] UNIQUE ([MacAddress]),
-   );
+      CREATE TABLE [Device]
+      (
+         [Id] INT IDENTITY(1, 1) NOT NULL,
+         [LocationId] INT NOT NULL,
+         [Name] NVARCHAR(30) NOT NULL,
+         [SerialNumber] NVARCHAR(30) NOT NULL,
+         [Type] SMALLINT NOT NULL,
+         [ModbusId] TINYINT NOT NULL,
+         [RsBoundRate] INT NOT NULL,
+         [RsDataBits] TINYINT NOT NULL,
+         [RsParity] TINYINT NOT NULL,
+         [RsStopBits] TINYINT NOT NULL,
+         [IncludeReport] BIT NOT NULL,
+         [IsActive] BIT NOT NULL,
+         [Version] ROWVERSION NOT NULL,
+         CONSTRAINT [PK_Device_Id] PRIMARY KEY CLUSTERED ([Id]),
+         CONSTRAINT [FK_Device_LocationId] FOREIGN KEY ([LocationId]) REFERENCES [Location]([Id]),
+         CONSTRAINT [UK_Device_LocationId_Name] UNIQUE ([LocationId], [Name]),
+         CONSTRAINT [UK_Device_LocationId_ModbusId] UNIQUE ([LocationId], [ModbusId]),
+      );
 
-   CREATE TABLE [LocationHistory]
-   (
-      [Id] INT IDENTITY(1, 1) NOT NULL,
-      [LocationId] INT NOT NULL,
-      [Name] NVARCHAR(30) NOT NULL,
-      [MacAddress] NVARCHAR(12) NOT NULL,
-      [IncludeReport] BIT NOT NULL,
-      [IsActive] BIT NOT NULL,
-      [CreatedById] INT NOT NULL,
-      [CreateDate] DATETIME2 NOT NULL,
-      CONSTRAINT [PK_LocationHistory_Id] PRIMARY KEY CLUSTERED ([Id]),
-      CONSTRAINT [FK_LocationHistory_LocationId] FOREIGN KEY ([LocationId]) REFERENCES [Location]([Id]),
-      CONSTRAINT [FK_LocationHistory_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [User]([Id]),
-      INDEX [IX_LocationHistory_LocationId] NONCLUSTERED ([LocationId]),
-   );
+      CREATE TABLE [DeviceHistory]
+      (
+         [Id] INT IDENTITY(1, 1) NOT NULL,
+         [DeviceId] INT NOT NULL,
+         [Name] NVARCHAR(30) NOT NULL,
+         [LocationName] NVARCHAR(30) NOT NULL,
+         [Type] NVARCHAR(30) NOT NULL,
+         [ModbusId] NVARCHAR(3) NOT NULL,
+         [RsBoundRate] NVARCHAR(10) NOT NULL,
+         [RsDataBits] NVARCHAR(5) NOT NULL,
+         [RsParity] NVARCHAR(10) NOT NULL,
+         [RsStopBits] NVARCHAR(10) NOT NULL,
+         [IncludeReport] BIT NOT NULL,
+         [IsActive] BIT NOT NULL,
+         [CreatedById] INT NOT NULL,
+         [CreateDate] DATETIME2 NOT NULL,
+         CONSTRAINT [PK_DeviceHistory_Id] PRIMARY KEY CLUSTERED ([Id]),
+         CONSTRAINT [FK_DeviceHistory_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
+         CONSTRAINT [FK_DeviceHistory_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [User]([Id]),
+         INDEX [IX_DeviceHistory_DeviceId] NONCLUSTERED ([DeviceId]),
+      );
 
-   CREATE TABLE [Device]
-   (
-      [Id] INT IDENTITY(1, 1) NOT NULL,
-      [LocationId] INT NOT NULL,
-      [Name] NVARCHAR(30) NOT NULL,
-      [SerialNumber] NVARCHAR(30) NOT NULL,
-      [Type] SMALLINT NOT NULL,
-      [ModbusId] TINYINT NOT NULL,
-      [RsBoundRate] INT NOT NULL,
-      [RsDataBits] TINYINT NOT NULL,
-      [RsParity] TINYINT NOT NULL,
-      [RsStopBits] TINYINT NOT NULL,
-      [IncludeReport] BIT NOT NULL,
-      [IsActive] BIT NOT NULL,
-      [Version] ROWVERSION NOT NULL,
-      CONSTRAINT [PK_Device_Id] PRIMARY KEY CLUSTERED ([Id]),
-      CONSTRAINT [FK_Device_LocationId] FOREIGN KEY ([LocationId]) REFERENCES [Location]([Id]),
-      CONSTRAINT [UK_Device_LocationId_Name] UNIQUE ([LocationId], [Name]),
-      CONSTRAINT [UK_Device_LocationId_ModbusId] UNIQUE ([LocationId], [ModbusId]),
-   );
+      CREATE TABLE [Meter]
+      (
+         [Date] DATETIME2 NOT NULL,
+         [DeviceId] INT NOT NULL,
+         [InletTemp] REAL NOT NULL,
+         [OutletTemp] REAL NOT NULL,
+         [Power] REAL NOT NULL,
+         [Volume] REAL NOT NULL,
+         [VolumeSummary] INT NOT NULL,
+         [EnergySummary] INT NOT NULL,
+         [HourCount] INT NOT NULL,
+         [ErrorCode] SMALLINT NOT NULL,
+         CONSTRAINT [PK_Meter_Date_DeviceId] PRIMARY KEY CLUSTERED ([Date], [DeviceId]),
+         CONSTRAINT [FK_Meter_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
+      ) WITH (DATA_COMPRESSION = PAGE);
 
-   CREATE TABLE [DeviceHistory]
-   (
-      [Id] INT IDENTITY(1, 1) NOT NULL,
-      [DeviceId] INT NOT NULL,
-      [Name] NVARCHAR(30) NOT NULL,
-      [LocationName] NVARCHAR(30) NOT NULL,
-      [Type] NVARCHAR(30) NOT NULL,
-      [ModbusId] NVARCHAR(3) NOT NULL,
-      [RsBoundRate] NVARCHAR(10) NOT NULL,
-      [RsDataBits] NVARCHAR(5) NOT NULL,
-      [RsParity] NVARCHAR(10) NOT NULL,
-      [RsStopBits] NVARCHAR(10) NOT NULL,
-      [IncludeReport] BIT NOT NULL,
-      [IsActive] BIT NOT NULL,
-      [CreatedById] INT NOT NULL,
-      [CreateDate] DATETIME2 NOT NULL,
-      CONSTRAINT [PK_DeviceHistory_Id] PRIMARY KEY CLUSTERED ([Id]),
-      CONSTRAINT [FK_DeviceHistory_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
-      CONSTRAINT [FK_DeviceHistory_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [User]([Id]),
-      INDEX [IX_DeviceHistory_DeviceId] NONCLUSTERED ([DeviceId]),
-   );
+      CREATE TABLE [Climatix]
+      (
+         [Date] DATETIME2 NOT NULL,
+         [DeviceId] INT NOT NULL,
+         [OutsideTemp] REAL NOT NULL,
+         [CoHighInletPresure] REAL NOT NULL,
+         [CoHighOutletPresure] REAL NOT NULL,
+         [Alarm] BIT NOT NULL,
+         [Co1LowInletTemp] REAL NOT NULL,
+         [Co1LowOutletTemp] REAL NOT NULL,
+         [Co1LowOutletPresure] REAL NOT NULL,
+         [Co1HeatCurveTemp] REAL NOT NULL,
+         [Co1PumpAlarm] BIT NOT NULL,
+         [Co1PumpStatus] BIT NOT NULL,
+         [Co1ValvePosition] TINYINT NOT NULL,
+         [Co1Status] BIT NOT NULL,
+         [Co2LowInletTemp] REAL NOT NULL,
+         [Co2LowOutletTemp] REAL NOT NULL,
+         [Co2LowOutletPresure] REAL NOT NULL,
+         [Co2HeatCurveTemp] REAL NOT NULL,
+         [Co2PumpAlarm] BIT NOT NULL,
+         [Co2PumpStatus] BIT NOT NULL,
+         [Co2ValvePosition] TINYINT NOT NULL,
+         [Co2Status] BIT NOT NULL,
+         [CwuTemp] REAL NOT NULL,
+         [CwuTempSet] REAL NOT NULL,
+         [CwuPumpAlarm] BIT NOT NULL,
+         [CwuPumpStatus] BIT NOT NULL,
+         [CwuValvePosition] TINYINT NOT NULL,
+         [CwuStatus] BIT NOT NULL,
+         CONSTRAINT [PK_Climatix_Date_DeviceId] PRIMARY KEY CLUSTERED ([Date], [DeviceId]),
+         CONSTRAINT [FK_Climatix_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
+      ) WITH (DATA_COMPRESSION = PAGE);
 
-   CREATE TABLE [Meter]
-   (
-      [Date] DATETIME2 NOT NULL,
-      [DeviceId] INT NOT NULL,
-      [InletTemp] REAL NOT NULL,
-      [OutletTemp] REAL NOT NULL,
-      [Power] REAL NOT NULL,
-      [Volume] REAL NOT NULL,
-      [VolumeSummary] INT NOT NULL,
-      [EnergySummary] INT NOT NULL,
-      [HourCount] INT NOT NULL,
-      [ErrorCode] SMALLINT NOT NULL,
-      CONSTRAINT [PK_Meter_Date_DeviceId] PRIMARY KEY CLUSTERED ([Date], [DeviceId]),
-      CONSTRAINT [FK_Meter_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
-   ) WITH (DATA_COMPRESSION = PAGE);
+      CREATE TABLE [Rvd145]
+      (
+         [Date] DATETIME2 NOT NULL,
+         [DeviceId] INT NOT NULL,
+         [OutsideTemp] REAL NOT NULL,
+         [CoHighInletPresure] REAL NOT NULL,
+         [Alarm] SMALLINT NOT NULL,
+         [Co1HighOutletTemp] REAL NOT NULL,
+         [Co1LowInletTemp] REAL NOT NULL,
+         [Co1LowOutletPresure] REAL NOT NULL,
+         [Co1HeatCurveTemp] REAL NOT NULL,
+         [Co1PumpStatus] BIT NOT NULL,
+         [Co1Status] BIT NOT NULL,
+         [CwuTemp] REAL NOT NULL,
+         [CwuTempSet] REAL NOT NULL,
+         [CwuCirculationTemp] REAL NOT NULL,
+         [CwuPumpStatus] BIT NOT NULL,
+         [CwuStatus] BIT NOT NULL,
+         CONSTRAINT [PK_Rvd145_Date_DeviceId] PRIMARY KEY CLUSTERED ([Date], [DeviceId]),
+         CONSTRAINT [FK_Rvd145_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
+      ) WITH (DATA_COMPRESSION = PAGE);
 
-   CREATE TABLE [Climatix]
-   (
-      [Date] DATETIME2 NOT NULL,
-      [DeviceId] INT NOT NULL,
-      [OutsideTemp] REAL NOT NULL,
-      [CoHighInletPresure] REAL NOT NULL,
-      [CoHighOutletPresure] REAL NOT NULL,
-      [Alarm] BIT NOT NULL,
-      [Co1LowInletTemp] REAL NOT NULL,
-      [Co1LowOutletTemp] REAL NOT NULL,
-      [Co1LowOutletPresure] REAL NOT NULL,
-      [Co1HeatCurveTemp] REAL NOT NULL,
-      [Co1PumpAlarm] BIT NOT NULL,
-      [Co1PumpStatus] BIT NOT NULL,
-      [Co1ValvePosition] TINYINT NOT NULL,
-      [Co1Status] BIT NOT NULL,
-      [Co2LowInletTemp] REAL NOT NULL,
-      [Co2LowOutletTemp] REAL NOT NULL,
-      [Co2LowOutletPresure] REAL NOT NULL,
-      [Co2HeatCurveTemp] REAL NOT NULL,
-      [Co2PumpAlarm] BIT NOT NULL,
-      [Co2PumpStatus] BIT NOT NULL,
-      [Co2ValvePosition] TINYINT NOT NULL,
-      [Co2Status] BIT NOT NULL,
-      [CwuTemp] REAL NOT NULL,
-      [CwuTempSet] REAL NOT NULL,
-      [CwuPumpAlarm] BIT NOT NULL,
-      [CwuPumpStatus] BIT NOT NULL,
-      [CwuValvePosition] TINYINT NOT NULL,
-      [CwuStatus] BIT NOT NULL,
-      CONSTRAINT [PK_Climatix_Date_DeviceId] PRIMARY KEY CLUSTERED ([Date], [DeviceId]),
-      CONSTRAINT [FK_Climatix_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
-   ) WITH (DATA_COMPRESSION = PAGE);
-
-   CREATE TABLE [Rvd145]
-   (
-      [Date] DATETIME2 NOT NULL,
-      [DeviceId] INT NOT NULL,
-      [OutsideTemp] REAL NOT NULL,
-      [CoHighInletPresure] REAL NOT NULL,
-      [Alarm] SMALLINT NOT NULL,
-      [Co1HighOutletTemp] REAL NOT NULL,
-      [Co1LowInletTemp] REAL NOT NULL,
-      [Co1LowOutletPresure] REAL NOT NULL,
-      [Co1HeatCurveTemp] REAL NOT NULL,
-      [Co1PumpStatus] BIT NOT NULL,
-      [Co1Status] BIT NOT NULL,
-      [CwuTemp] REAL NOT NULL,
-      [CwuTempSet] REAL NOT NULL,
-      [CwuCirculationTemp] REAL NOT NULL,
-      [CwuPumpStatus] BIT NOT NULL,
-      [CwuStatus] BIT NOT NULL,
-      CONSTRAINT [PK_Rvd145_Date_DeviceId] PRIMARY KEY CLUSTERED ([Date], [DeviceId]),
-      CONSTRAINT [FK_Rvd145_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Device]([Id]),
-   ) WITH (DATA_COMPRESSION = PAGE);
-
-   COMMIT TRANSACTION;
-END TRY
-BEGIN CATCH
-   ROLLBACK TRANSACTION;
-END CATCH;
+      COMMIT TRANSACTION;
+   END TRY
+   BEGIN CATCH
+      ROLLBACK TRANSACTION;
+   END CATCH;
+END;
