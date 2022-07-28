0249baf8b1d63e5c1e28aa689f8e7b3c6a01bb17
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2022-07-28 11:14:03 +0200

    Change code indent

--- src/Zeus.Domain/Devices/Device.cs
+++ src/Zeus.Domain/Devices/Device.cs
@@ -18,37 +18,33 @@ namespace Zeus.Domain.Devices
       public StopBits RsStopBits { get; private set; }
       public bool IncludeReport { get; private set; }
 
-      public PlcType PlcType =>
-         Type switch
-         {
-            DeviceType.Kamstrup or DeviceType.KamstrupRs500 => PlcType.Meter,
-            DeviceType.ClimatixCo or DeviceType.ClimatixCoCo or DeviceType.ClimatixCoCwu => PlcType.Climatix,
-            DeviceType.Rvd145Co or DeviceType.Rvd145CoCo or DeviceType.Rvd145CoCwu => PlcType.Rvd145,
-            _ => PlcType.None
-         };
+      public PlcType PlcType => Type switch
+      {
+         DeviceType.Kamstrup or DeviceType.KamstrupRs500 => PlcType.Meter,
+         DeviceType.ClimatixCo or DeviceType.ClimatixCoCo or DeviceType.ClimatixCoCwu => PlcType.Climatix,
+         DeviceType.Rvd145Co or DeviceType.Rvd145CoCo or DeviceType.Rvd145CoCwu => PlcType.Rvd145,
+         _ => PlcType.None
+      };
 
-      public bool IsPlc =>
-         Type switch
-         {
-            DeviceType.Kamstrup or DeviceType.KamstrupRs500 => false,
-            _ => true
-         };
+      public bool IsPlc => Type switch
+      {
+         DeviceType.Kamstrup or DeviceType.KamstrupRs500 => false,
+         _ => true
+      };
 
       public bool IsCo1 => IsPlc;
 
-      public bool IsCo2 =>
-         Type switch
-         {
-            DeviceType.ClimatixCoCo or DeviceType.Rvd145CoCo => true,
-            _ => false
-         };
+      public bool IsCo2 => Type switch
+      {
+         DeviceType.ClimatixCoCo or DeviceType.Rvd145CoCo => true,
+         _ => false
+      };
 
-      public bool IsCwu =>
-         Type switch
-         {
-            DeviceType.ClimatixCoCwu or DeviceType.Rvd145CoCwu => true,
-            _ => false
-         };
+      public bool IsCwu => Type switch
+      {
+         DeviceType.ClimatixCoCwu or DeviceType.Rvd145CoCwu => true,
+         _ => false
+      };
 
       public Device(int locationId, string name, DeviceType type, byte modbusId, BoundRate rsBoundRate, DataBits rsDataBits, Parity rsParity, StopBits rsStopBits, bool includeReport, bool isActive) : base(isActive)
       {
